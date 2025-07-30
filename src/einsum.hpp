2658f6081b72575eb2abc4ecb35add7eadbe96f9
danila seliayeu <dan.seliayeu@gmail.com>
2025-07-29 19:01:42 -0600

    add utils for constructing tree from opt einsum benchmark

---
+++ src/einsum.hpp
@@ -0,0 +1,116 @@
+#pragma once
+#include "dot.hpp"
+#include "graph.hpp"
+#include "utils.hpp"
+#include "taco.h"
+#include "taco/format.h"
+#include <vector>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include "graph.hpp"
+
+std::string extractOutputs(std::string const &einsumString) {
+  int arrowPos = einsumString.find("->");
+  return einsumString.substr(arrowPos + 2);
+}
+
+std::vector<std::string> extractInputs(std::string const &einsumString) {
+  std::vector<std::string> inputs;
+  int arrowPos = einsumString.find("->");
+  std::string lhs = einsumString.substr(0, arrowPos);
+
+  std::stringstream ss(lhs);
+  std::string token;
+  while (std::getline(ss, token, ','))
+    inputs.push_back(token);
+
+  return inputs;
+}
+
+std::unordered_map<char, int> constructSizeMap(
+  std::vector<std::string> const &inputs,
+  std::vector<std::vector<int>> const &tensorSizes)
+{
+  std::unordered_map<char, int> sizeMap;
+
+  for (int i = 0; i < tensorSizes.size(); ++i) {
+    for (int j = 0; j < tensorSizes.size(); ++j) {
+      auto indVar = inputs[i][j];
+      auto dimSize = tensorSizes[i][j];
+      sizeMap[indVar] = dimSize;
+    }
+  }
+  return sizeMap;
+
+}
+
+std::vector<int> deduceOutputDims(std::string const &einsumString,
+  std::vector<int> const &sizes1, std::vector<int> const &sizes2)
+{
+  std::string output = extractOutputs(einsumString);
+  std::vector<std::string> inputs = extractInputs(einsumString);
+  std::unordered_map<char, int> sizeMap = constructSizeMap(inputs, {sizes1, sizes2});
+  std::vector<int> outputSizes;
+
+  for (auto indVar : output)
+    outputSizes.push_back(sizeMap[indVar]);
+
+  return outputSizes;
+}
+
+Graph buildTree(std::vector<std::vector<int>> tensorSizes, 
+  std::vector<std::string> const &contractionStrings,
+  std::vector<std::pair<int, int>> const &contractionInds)
+{
+  std::vector<TensorPtr> tensors;
+  std::vector<TensorPtr> tensorStack;
+  std::vector<OpNodePtr> ops;
+
+  // construct tensors based on tensorSizes
+  int ind = 0;
+  for (auto dims : tensorSizes) {
+    auto denseSparsityVector = generate_sparsity_vector(0.0, size);
+    std::vector<bitset> sparsityVectors;
+    for (auto dim : dims) {
+      sparsityVectors.push_back(generate_sparsity_vector(0.0, dim));
+    }
+    auto newTensor = std::make_shared<Tensor>(
+      dims,
+      sparsityVectors,
+      std::to_string(ind++));
+    tensors.push_back(newTensor);
+    tensorStack.push_back(newTensor);
+  }
+
+  // add einsum to list of ops and new tensor to tensors vector
+  for (int i = 0; i < contractionStrings.size(); ++i) {
+    int ind1 = contractionInds[i].first < contractionInds[i].second 
+      ? contractionInds[i].first : contractionInds[i].second;
+    int ind2 = contractionInds[i].first > contractionInds[i].second 
+      ? contractionInds[i].first : contractionInds[i].second;
+    std::vector<int> outputDims = deduceOutputDims(
+        contractionStrings[i], tensorStack[ind1]->sizes, tensorStack[ind2]->sizes);
+
+    std::vector<bitset> sparsityVectors;
+    for (auto dim : outputDims)
+      sparsityVectors.push_back(generate_sparsity_vector(0.0, dim));
+
+    auto newTensor = std::make_shared<Tensor>(
+      outputDims,
+      sparsityVectors,
+      std::to_string(ind++));
+
+    tensors.push_back(newTensor);
+    ops.push_back(std::make_shared<Einsum>(
+      std::vector<TensorPtr>{ tensorStack[ind1], tensorStack[ind2] }, 
+      tensors.back(),
+      contractionStrings[i]
+    ));
+    tensorStack.erase(tensorStack.begin() + ind2);
+    tensorStack.erase(tensorStack.begin() + ind1);
+    tensorStack.push_back(tensors.back());
+  }
+
+  return Graph::build_graph(tensors, tensorStack[0], ops);
+}
