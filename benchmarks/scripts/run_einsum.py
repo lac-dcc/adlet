61295ff53f1ef6dfa56a2ebe41ad8f222db90647
Kaio Henrique <deandradekaio@gmail.com>
2025-08-09 18:23:27 -0300

    feat: add repeats to the einsum benchmark

--- benchmarks/scripts/run_einsum.py
+++ benchmarks/scripts/run_einsum.py
@@ -1,11 +1,7 @@
+import statistics
 import sys
 import os
 import subprocess
-
-
-SPARSITY = 0.5
-PROB_TO_PRUNE = 0.5
-SEED = 4
 
 
 def parse_output(output):
@@ -30,29 +26,37 @@ def parse_output(output):
             metrics["tensors-size"] = float(line.split("=")[-1].strip())
     return metrics
 
-def run(benchmark_dir: str):
+def run(benchmark_dir: str, sparsity: float, prob_to_prune: float, seed: int, n: int):
     files = os.listdir(benchmark_dir)
-    with open("result.txt", "wt") as result_file:
+    with open(f"result{sparsity}{prob_to_prune}{seed}{n}.txt", "wt") as result_file:
         result_file.write('file_name,sparsity,prob_to_prune,propagate,ratio_before,after,analysis,load_time,compilation_time,run_time, overall_memory, tensors-size\n')
-
         for file in files:
             print(f"running {file}")
-            file_path = f"./sub100/{file}"
+            file_path = f"{benchmark_dir}{file}"
             for propagate in [0, 1]:
-                cmd = ["./benchmark", "einsum", file_path, str(SPARSITY), str(PROB_TO_PRUNE), str(propagate), str(SEED)]
+                cmd = ["./benchmark", "einsum", file_path, str(sparsity), str(prob_to_prune), str(propagate), str(seed)]
+                times = {"before":[], "after": [], "analysis": [], "load": [], "compilation": [], "runtime": [], "memory": [], "tensors-size":[]}
                 try:
-                    process = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                    process.wait()
-                    #result = subprocess.run(cmd, capture_output=True, text=True)
-                    mean_metrics = parse_output(process.stdout.read())
+                    for i in range(n):
+                        print(f"iteration {i}/{n}",  end="\r")
+                        process = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                        process.wait()
+                        metrics = parse_output(process.stdout.read())
+                        for k in times:
+                            times[k].append(metrics.get(k, 0.0))
 
-                    result_line = f'{file},{SPARSITY}, {PROB_TO_PRUNE}, {propagate}, {mean_metrics["before"]}, {mean_metrics["after"]}, {mean_metrics["analysis"]}, {mean_metrics["load"]}, {mean_metrics["compilation"]}, {mean_metrics["runtime"]}, {mean_metrics["memory"]}, {mean_metrics["tensors-size"]}'
+                    mean_metrics = {k: statistics.mean(times[k]) for k in times}
+                    result_line = f'{file},{sparsity}, {prob_to_prune}, {propagate}, {mean_metrics["before"]}, {mean_metrics["after"]}, {mean_metrics["analysis"]}, {mean_metrics["load"]}, {mean_metrics["compilation"]}, {mean_metrics["runtime"]}, {mean_metrics["memory"]}, {mean_metrics["tensors-size"]}'
                     result_file.write(result_line + "\n")
-                except:
-                    print(f"Error running {file_path}")
-
+                except Exception as e:
+                    print(f"Error running {file_path}: {str(e)}")
 
 
 if __name__ == "__main__":
     benchmark_dir = sys.argv[1]
-    run(benchmark_dir)
+    sparsity = float(sys.argv[2])
+    prob_to_prune = float(sys.argv[3])
+    seed = int(sys.argv[4])
+    repeats = int(sys.argv[5])
+    run(benchmark_dir, sparsity, prob_to_prune, seed, repeats)
+
