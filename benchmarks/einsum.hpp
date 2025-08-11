8bc9bdf3f48ebcab211462e5ddbae7b495d5fa5f
Kaio Henrique <deandradekaio@gmail.com>
2025-08-11 14:03:39 -0300

    feat: add format as a parameter

--- benchmarks/einsum.hpp
+++ benchmarks/einsum.hpp
@@ -4,7 +4,8 @@
 #include "taco/format.h"
 
 void run(const std::string &file_path, const bool propagate,
-         const double sparsity, const double chanceToPrune) {
+         const double sparsity, const double chanceToPrune,
+         const bool sparse = true) {
 
   auto benchmark = readEinsumBenchmark(file_path);
 
@@ -32,7 +33,7 @@ void run(const std::string &file_path, const bool propagate,
   for (auto t : g.inputs) {
     if (!t->outputTensor) {
       /*t->create_data(taco::Format({taco::Sparse, taco::Sparse}));*/
-      t->create_data(generateModes(t->numDims, true));
+      t->create_data(generateModes(t->numDims, sparse));
       t->initialize_data();
     } else
       t->create_data(0.5);
@@ -54,19 +55,22 @@ void run(const std::string &file_path, const bool propagate,
 }
 
 int benchmark_einsum(int argc, char *argv[]) {
-  if (argc != 7) {
+  if (argc != 8) {
     std::cerr << "Usage: " << argv[0]
               << " einsum <file_path> <sparsity> <chance_to_prune> "
-                 "<propagate> <random_seed>\n ";
+                 "<sparse> <propagate> <random_seed>\n ";
     return 1;
   }
   int param = 1;
   const std::string file_path = argv[++param];
-  double sparsity = std::stod(argv[++param]);
-  double chanceToPrune = std::stod(argv[++param]);
-  bool propagate = std::stoi(argv[++param]);
+  const std::string sparseStr = argv[++param];
+  const bool sparse = sparseStr == "sparse";
+  const double sparsity = std::stod(argv[++param]);
+  const double chanceToPrune = std::stod(argv[++param]);
+  const bool propagate = std::stoi(argv[++param]);
   SEED = std::stoi(argv[++param]);
-  run(file_path, propagate, sparsity, chanceToPrune);
+  std::cout << sparse << std::endl;
+  run(file_path, propagate, sparsity, chanceToPrune, sparse);
 
   return 0;
 }
