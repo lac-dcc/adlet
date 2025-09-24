dffe6ba7463e28ee42bfabf15715c8454c4ee6c2
Kaio Henrique <deandradekaio@gmail.com>
2025-09-24 10:00:04 -0300

    feat: 3d tensors

--- scripts/run_formats.py
+++ scripts/run_formats.py
@@ -32,8 +32,36 @@ def run_matrix():
                     except Exception as e:
                         print(f"Error {e} running cmd {cmd}")
 
+def run_3D():
+    rank = 3
+    with open("matrix.csv", "wt") as file:
+        for size in sizes:
+            for sparsity in sparsities:
+                for format in formats:
+                    cmd = ["./benchmark",
+                        "format",
+                        "SSS" if format == "S" else "DDD",
+                        str(rank),
+                        str(size),
+                        str(size),
+                        str(size),
+                        str(sparsity),
+                        str(sparsity),
+                        str(sparsity),
+                    ]
+                    print(cmd)
+                    try:
+                        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+                        process = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                        process.wait()
+                        file.write(process.stdout.read())
+                        file.flush()
+
+                    except Exception as e:
+                        print(f"Error {e} running cmd {cmd}")
 if __name__ == "__main__":
     run_matrix()
+    run_3D()
 
 
 
